761bb9f6f1f05ea7276479266e67b48bd161285e
Vinson Beduya <vinsonemb.151994@gmail.com>
2021-10-07 21:15:09 +1300

    Added comments.

--- src/BalancedPersistentDynamicSet/BPDS.java
+++ src/BalancedPersistentDynamicSet/BPDS.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Balanced Persistent Dynamic Set class that extends from Persistent Dynamic 
+ *  Set class. Uses a hook method for color in the BST class.
+ *
+ *  References used:
+ *  - https://algorithmtutor.com/Data-Structures/Tree/Red-Black-Trees/
+ *  - https://www.youtube.com/watch?v=5IBxA-bZZH8
  */
 package BalancedPersistentDynamicSet;
 
@@ -16,7 +19,7 @@ import java.util.Queue;
 /**
  * Uses Red Black Tree to keep it balanced.
  *
- * @author jcvsa
+ * @author Vinson
  * @param <E>
  */
 public class BPDS<E extends Comparable> extends PersistentDynamicSet<E> {
@@ -29,6 +32,12 @@ public class BPDS<E extends Comparable> extends PersistentDynamicSet<E> {
         super(c);
     }
 
+    /**
+     * Modified updateVersion from Persistent Dynamic Set and augmented it to
+     * make it work with a Red Black Tree.
+     *
+     * @param node
+     */
     @Override
     protected void updateVersion(Node node) {
         System.out.println("________________BUILDING________________");
@@ -89,12 +98,12 @@ public class BPDS<E extends Comparable> extends PersistentDynamicSet<E> {
         System.out.println(toString(newVersion));
         System.out.println(hashCodeString(newVersion));
 
+        rootNode = newVersion.clone();
+
         // Recolor and balance the tree.
         Queue<Node> clonedLatestVersion = new LinkedList<>(latestVersion);
-        fixTree(clonedLatestVersion);
+        balanceInsert(clonedLatestVersion);
 
-        rootNode = newVersion.clone();
-        
         // Make sure root node is black
         if (rootNode != null) {
             rootNode.setColor(Color.BLACK);
@@ -103,31 +112,45 @@ public class BPDS<E extends Comparable> extends PersistentDynamicSet<E> {
         versions.add(newVersion);
     }
 
-    private void fixTree(Queue<Node> latestVersion) {
+    /**
+     * Method to balance the tree when inserting. This is to make sure nodes
+     * adhere to the rules of a Red Black Tree. Modified the implementation from
+     * algorithm tutor to not use parent links and to base it off our Persistent
+     * Dynamic Set class.
+     *
+     * @param latestVersion
+     */
+    private void balanceInsert(Queue<Node> latestVersion) {
 
         Node current = latestVersion.poll();
         Node parent = latestVersion.poll();
         Node grandParent = latestVersion.poll();
 
-        if (current == null || parent == null || grandParent == null) {
+        if (parent == null) {
+            current.setColor(Color.BLACK);
+            return;
+        }
+
+        if (grandParent == null) {
             return;
         }
 
         while (parent.getColor().equals(Color.RED)) {
-            if (grandParent == null) {
-                break;
-            }
             if (parent.equals(grandParent.getRight())) {
                 Node uncle = grandParent.getLeft();
 
                 if (uncle != null && uncle.getColor().equals(Color.RED)) {
-                    uncle.setColor(Color.BLACK);
-                    parent.setColor(Color.BLACK);
+                    grandParent.getLeft().setColor(Color.BLACK);
+                    grandParent.getRight().setColor(Color.BLACK);
                     grandParent.setColor(Color.RED);
 
                     current = grandParent;
                     parent = latestVersion.poll();
                     grandParent = latestVersion.poll();
+
+                    if (parent == null) {
+                        break;
+                    }
                 } else {
                     if (current.equals(parent.getLeft())) {
                         current = parent;
@@ -137,23 +160,27 @@ public class BPDS<E extends Comparable> extends PersistentDynamicSet<E> {
                     }
 
                     parent.setColor(Color.BLACK);
-                    if (grandParent != null) {
-                        grandParent.setColor(Color.RED);
-                        leftRotate(grandParent, latestVersion.peek());
+                    if (grandParent == null) {
+                        break;
                     }
-
+                    grandParent.setColor(Color.RED);
+                    leftRotate(grandParent, latestVersion.peek());
                 }
             } else {
                 Node uncle = grandParent.getRight();
 
                 if (uncle != null && uncle.getColor().equals(Color.RED)) {
-                    uncle.setColor(Color.BLACK);
-                    parent.setColor(Color.BLACK);
+                    grandParent.getLeft().setColor(Color.BLACK);
+                    grandParent.getRight().setColor(Color.BLACK);
                     grandParent.setColor(Color.RED);
 
                     current = grandParent;
                     parent = latestVersion.poll();
                     grandParent = latestVersion.poll();
+
+                    if (parent == null) {
+                        break;
+                    }
                 } else {
                     if (current.equals(parent.getLeft())) {
                         current = parent;
@@ -163,69 +190,106 @@ public class BPDS<E extends Comparable> extends PersistentDynamicSet<E> {
                     }
 
                     parent.setColor(Color.BLACK);
-                    if (grandParent != null) {
-                        grandParent.setColor(Color.RED);
-                        rightRotate(grandParent, latestVersion.peek());
+                    if (grandParent == null) {
+                        break;
                     }
+                    grandParent.setColor(Color.RED);
+                    rightRotate(grandParent, latestVersion.peek());
                 }
             }
 
             if (current.equals(rootNode)) {
                 break;
             }
-
-            if (parent == null) {
-                break;
-            }
         }
     }
 
+    /**
+     * Performs right rotation of the tree without using an implicit parent
+     * reference from the Node class.
+     *
+     * @param node
+     * @param parent
+     */
     public void rightRotate(Node node, Node parent) {
-        if (node.getLeft() == null) {
+        if (node.getLeft() == null || node.getLeft().getRight() == null) {
             return;
         }
-
         Node newParent = node.getLeft();
-        Node tempRight = newParent.getRight();
-
-        if (parent != null) {
-            if (parent.getLeft().equals(node)) {
-                parent.setLeft(newParent);
-            } else {
-                parent.setRight(newParent);
-            }
-        }
-
-        newParent.setRight(node);
-        node.setLeft(tempRight);
-
+        node.setLeft(newParent.getRight());
         if (parent == null) {
             rootNode = newParent;
+        } else if (node == parent.getRight()) {
+            parent.setRight(newParent);
+        } else {
+            parent.setLeft(newParent);
         }
+        newParent.setRight(node);
+//        if (node.getLeft() == null) {
+//            return;
+//        }
+//
+//        Node newParent = node.getLeft();
+//        Node tempRight = newParent.getRight();
+//
+//        if (parent != null) {
+//            if (parent.getLeft().equals(node)) {
+//                parent.setLeft(newParent);
+//            } else {
+//                parent.setRight(newParent);
+//            }
+//        }
+//
+//        newParent.setRight(node);
+//        node.setLeft(tempRight);
+//
+//        if (parent == null) {
+//            rootNode = newParent;
+//        }
     }
 
+    /**
+     * Performs left rotation of the tree without using an implicit parent
+     * reference from the Node class.
+     *
+     * @param node
+     * @param parent
+     */
     public void leftRotate(Node node, Node parent) {
-        if (node.getRight() == null) {
+        if (node.getRight() == null || node.getRight().getLeft() == null) {
             return;
         }
-
         Node newParent = node.getRight();
-        Node tempLeft = newParent.getLeft();
-
-        if (parent != null) {
-            if (parent.getLeft().equals(node)) {
-                parent.setLeft(newParent);
-            } else {
-                parent.setRight(newParent);
-            }
-        }
-
-        newParent.setLeft(node);
-        node.setRight(tempLeft);
-
+        node.setRight(newParent.getLeft());
         if (parent == null) {
             rootNode = newParent;
+        } else if (node == parent.getRight()) {
+            parent.setRight(newParent);
+        } else {
+            parent.setLeft(newParent);
         }
+        newParent.setLeft(node);
+//        if (node.getRight() == null) {
+//            return;
+//        }
+//
+//        Node newParent = node.getRight();
+//        Node tempLeft = newParent.getLeft();
+//
+//        if (parent != null) {
+//            if (parent.getLeft().equals(node)) {
+//                parent.setLeft(newParent);
+//            } else {
+//                parent.setRight(newParent);
+//            }
+//        }
+//
+//        newParent.setLeft(node);
+//        node.setRight(tempLeft);
+//
+//        if (parent == null) {
+//            rootNode = newParent;
+//        }
     }
 
     @Override
